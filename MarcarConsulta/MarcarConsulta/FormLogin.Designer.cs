c3f045484cc336cd3a060a2a0096dacb477a4635
JuanSoftware <js33neospro@gmail.com>
2023-10-28 09:35:39 -0400

    Pagina de Login
    Fiz uma pagina de Login, Dependendo do Checkbox selecionado a busca é feita em tabelas diferentes no banco de dados.

---
+++ MarcarConsulta/MarcarConsulta/FormLogin.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace MarcarConsulta
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkboxCliente = new System.Windows.Forms.RadioButton();
+            this.checkboxADM = new System.Windows.Forms.RadioButton();
+            this.checkboxMedico = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.Location = new System.Drawing.Point(463, 336);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(136, 68);
+            this.btnEntrar.TabIndex = 0;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(147, 336);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(166, 68);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkboxMedico);
+            this.groupBox1.Controls.Add(this.checkboxADM);
+            this.groupBox1.Controls.Add(this.checkboxCliente);
+            this.groupBox1.Location = new System.Drawing.Point(273, 215);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Login:";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(154, 96);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(445, 22);
+            this.txtLogin.TabIndex = 4;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(154, 175);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(445, 22);
+            this.txtSenha.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(147, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Senha:";
+            // 
+            // checkboxCliente
+            // 
+            this.checkboxCliente.AutoSize = true;
+            this.checkboxCliente.Checked = true;
+            this.checkboxCliente.Location = new System.Drawing.Point(6, 21);
+            this.checkboxCliente.Name = "checkboxCliente";
+            this.checkboxCliente.Size = new System.Drawing.Size(69, 20);
+            this.checkboxCliente.TabIndex = 3;
+            this.checkboxCliente.TabStop = true;
+            this.checkboxCliente.Text = "Cliente";
+            this.checkboxCliente.UseVisualStyleBackColor = true;
+            // 
+            // checkboxADM
+            // 
+            this.checkboxADM.AutoSize = true;
+            this.checkboxADM.Location = new System.Drawing.Point(6, 47);
+            this.checkboxADM.Name = "checkboxADM";
+            this.checkboxADM.Size = new System.Drawing.Size(58, 20);
+            this.checkboxADM.TabIndex = 4;
+            this.checkboxADM.TabStop = true;
+            this.checkboxADM.Text = "ADM";
+            this.checkboxADM.UseVisualStyleBackColor = true;
+            // 
+            // checkboxMedico
+            // 
+            this.checkboxMedico.AutoSize = true;
+            this.checkboxMedico.Location = new System.Drawing.Point(6, 73);
+            this.checkboxMedico.Name = "checkboxMedico";
+            this.checkboxMedico.Size = new System.Drawing.Size(73, 20);
+            this.checkboxMedico.TabIndex = 5;
+            this.checkboxMedico.TabStop = true;
+            this.checkboxMedico.Text = "Medico";
+            this.checkboxMedico.UseVisualStyleBackColor = true;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 453);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnEntrar);
+            this.Name = "FormLogin";
+            this.Text = "FormLogin";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton checkboxCliente;
+        private System.Windows.Forms.RadioButton checkboxMedico;
+        private System.Windows.Forms.RadioButton checkboxADM;
+    }
+}
---
+++ MarcarConsulta/MarcarConsulta/FormLogin.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarcarConsulta
+{
+    public partial class FormLogin : Form
+    {
+        string connectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=Vida;Integrated Security=True";
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            string login = txtLogin.Text;
+            string senha = txtSenha.Text;
+            string checkboxSelecionada = GetSelectedCheckBox();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "";
+                string tableName = "";
+
+                // Determine a tabela com base na seleção do checkbox
+                if (checkboxSelecionada == "Cliente")
+                {
+                    tableName = "Client";
+                }
+                else if (checkboxSelecionada == "ADM")
+                {
+                    tableName = "Administrator";
+                }
+                else if (checkboxSelecionada == "Medico")
+                {
+                    tableName = "Doctor";
+                }
+
+                query = $"SELECT * FROM {tableName} WHERE Login = @Login AND Password = @Senha";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Login", login);
+                    command.Parameters.AddWithValue("@Senha", senha);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Login bem-sucedido, determina o tipo de usuário
+                            // Não é necessário mais verificar "Tipo" no banco de dados
+                            // A informação está implícita com base na tabela consultada
+
+                            // Fecha o formulário de login
+                            this.Hide();
+
+                            // Abre o formulário correspondente ao tipo de usuário
+                            if (checkboxSelecionada == "Cliente")
+                            {
+                                FormCliente formCliente = new FormCliente();
+                                formCliente.ShowDialog();
+                            }
+                            else if (checkboxSelecionada == "ADM")
+                            {
+                                FormADM formADM = new FormADM();
+                                formADM.ShowDialog();
+                            }
+                            else if (checkboxSelecionada == "Medico")
+                            {
+                                FormMedico formMedico = new FormMedico();
+                                formMedico.ShowDialog();
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Usuário não encontrado no banco de dados.");
+                            LimparCampos();
+                        }
+                    }
+                }
+            }
+        }
+
+        private string GetSelectedCheckBox()
+        {
+            if (checkboxCliente.Checked)
+                return "Cliente";
+            else if (checkboxADM.Checked)
+                return "ADM";
+            else if (checkboxMedico.Checked)
+                return "Medico";
+            else
+                return "";
+        }
+
+        private void LimparCampos()
+        {
+            txtLogin.Clear();
+            txtSenha.Clear();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+
+
+
+
+        
+    
+}
